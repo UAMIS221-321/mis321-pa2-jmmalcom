9103718c3bf9f0e73f5ae47da0a4db74df5d6a09
jmmalcom <jmmalcom@crimson.ua.edu>
2022-02-28 14:37:38 -0600

    PA2

--- BattleArena.cs
+++ BattleArena.cs
@@ -13,7 +13,7 @@ namespace mis321_pa2_jmmalcom
                 Console.WriteLine($"{player1.Name} gets to go first.");
                 double attackDamage = player1.attackBehavior.Attack(player1, player2);
                 Console.WriteLine($"{player1.Name} your attack strength is {player1.AttackStrength}. You dealt {Math.Round(attackDamage)} damage to the opponent {player2.Name}.");
-                Console.WriteLine($"{player2.Name} your current health is {Math.Round(player2.Health)}. Your defense power is {player2.DefensivePower}.\nBased off your health, it will take {Math.Round(player2.Health/attackDamage)} hits before you lose.");
+                Console.WriteLine($"{player2.Name} your current health is {Math.Round(player2.Health)}. Your defense power is {player2.DefensivePower}.\nBased off your health, it will take {Math.Ceiling(player2.Health/attackDamage)} hits before you lose.");
                 count ++;
             }
 
@@ -29,7 +29,7 @@ namespace mis321_pa2_jmmalcom
                 Console.ReadKey();
                 double attackDamage = player2.attackBehavior.Attack(player2, player1);
                 Console.WriteLine($"{player2.Name} your attack strength is {player2.AttackStrength}. You dealt {Math.Round(attackDamage)} damage to the opponent {player1.Name}.");
-                Console.WriteLine($"{player1.Name} your current health is {Math.Round(player1.Health)}. Your defense power is {player1.DefensivePower}.\nBased off your health, it will take {Math.Round(player1.Health/attackDamage)} hits before you lose.");
+                Console.WriteLine($"{player1.Name} your current health is {Math.Round(player1.Health)}. Your defense power is {player1.DefensivePower}.\nBased off your health, it will take {Math.Ceiling(player1.Health/attackDamage)} hits before you lose.");
 
                 //Player 1 is going to attack.
                 if (player1.Health > 0)
@@ -37,8 +37,8 @@ namespace mis321_pa2_jmmalcom
                     Console.WriteLine($"it is {player1.Name}'s turn to attack. Press enter to attack.");
                     Console.ReadKey();
                     attackDamage = player1.attackBehavior.Attack(player1, player2);
-                    Console.WriteLine($"{player1.Name} your attack strength is {player1.AttackStrength}. You dealt {Math.Round(attackDamage)} damage to the opponent {player1.Name}.");
-                    Console.WriteLine($"{player2.Name} your current health is {Math.Round(player2.Health)}. Your defense power is {player2.DefensivePower}.\nBased off your health, it will take {Math.Round(player2.Health/attackDamage)} hits before you lose.");
+                    Console.WriteLine($"{player1.Name} your attack strength is {player1.AttackStrength}. You dealt {Math.Round(attackDamage)} damage to the opponent {player2.Name}.");
+                    Console.WriteLine($"{player2.Name} your current health is {Math.Round(player2.Health)}. Your defense power is {player2.DefensivePower}.\nBased off your health, it will take {Math.Ceiling(player2.Health/attackDamage)} hits before you lose.");
                 }
                 count ++;
 
